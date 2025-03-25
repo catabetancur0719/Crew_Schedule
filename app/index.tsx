3ee95947c605ed83b24505720eac4c664acd9942
catabetancur0719 <catalina.betancur0719@gmail.com>
2025-03-25 18:17:00 -0500

    added pilot and aircraft filters

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,45 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text, SafeAreaView } from "react-native";
+
+
+import PilotFilter from "../components/PilotFilter";
+import AirplaneFilter from "@/components/AirplaneFilter";
 
 
 export default function crewApp() {
   return (
-    <View style={styles.container}>
-      <PilotFilter /> 
-    </View>
+    <SafeAreaView style={styles.container}>
+
+      <Text style={styles.titleText}>Catarata Airways</Text>
+
+      <View style={styles.filters}>
+        <PilotFilter />
+        <AirplaneFilter />
+      </View>
+
+</SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#f0f0f0" },
+
+  container: { 
+    flex: 1, 
+
+  },
+
+  titleText:{
+    textAlign:"center",
+    fontSize: 20,
+    fontWeight:"bold",
+  },
+
+
+  filters:{
+    marginTop:20,
+    flexDirection:"row",
+    justifyContent: "space-around",
+    alignItems: "center",
+
+  }
 });
